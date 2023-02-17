006fb3b652e242fca024845bc678387dd5a6d01b
larramorelaura <may.lauraa@gmail.com>
2023-02-16 19:20:09 -0500

    updated img alt tag

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
         </div>
         <div className={styles.forScreenSize}>
             <div className={styles.imgContainer}>
-                <img className={styles.profilePic} src={profile} alt="picture of Laura Larramore5"/>
+                <img className={styles.profilePic} src={profile} alt="Laura Larramore"/>
             </div>
             <div className={styles.aboutDescription}>
                 <p>I'm a recently certified full stack developer with a background in education and technology support. Skilled in Python, JavaScript, and web frameworks like Flask and React, I've built apps that leverage technologies like Google Vision AI and Socket.io to deliver engaging, streamlined user experiences. I'm excited to bring my diverse skills to a software development role and continue growing in the field.</p>
